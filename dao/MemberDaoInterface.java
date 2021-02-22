c3de23cd9b60e1dfce4b6dcb61282d70cb91ca06
youngyoung0 <kxodud10054@gmail.com>
2021-02-22 12:41:27 +0900

    dao  메소드 설명 추가하였습니다.

--- dao/MemberDaoInterface.java
+++ dao/MemberDaoInterface.java
@@ -6,58 +6,111 @@ import vo.Member;
 import vo.Sign_Together;
 
 public interface MemberDaoInterface {
-	
-	//로그인 
-	//리턴값 : 회원정보
+
+	/** 로그인 
+	 *  @param id
+	 *  @param pwd
+	 *  @return  Member 멤버 객체 를 전달합니다. 
+	 */
 	public Member login(String id,String pwd)throws Exception;
 	
 	
-	//로그아웃
+	/**
+	 * 로그아웃
+	 * @param id
+	 * 
+	 */
 	public void logout(String id)throws Exception;
 	
 	
-	//회원가입
+	/**
+	 * 회원가입
+	 * @param m 멤버객체
+	 * 
+	 */
 	public void signup(Member m)throws Exception;
 	
-	
-	//아이디찾기
-	//리턴값 : ID
+
+	/**
+	 * 
+	 * @param birthday
+	 * @param phonme_num
+	 * @return id
+	 * 
+	 */
 	public String findId(Date birthday,String phonme_num)throws Exception;
 	
 	
-	//비밀번호찾기
-	//리턴값 : PWD
+	/**
+	 * 
+	 * @param id
+	 * @param phonme_num
+	 * @return password
+	 *
+	 */
 	public String findPwd(String id,String phonme_num)throws Exception;
 	
 	
-	//마이페이지
-	//리턴값 : 진행중인 챌린지
+
+	/**
+	 * 
+	 * @param id
+	 * @return sign_together 진행중인 투게더 객체
+	 * 
+	 */
 	public Sign_Together mypage(String id)throws Exception;
 	
 	
-	//정보수정
-	//리턴값 : 실패시 0 , 성공시 1 
+	/**
+	 * 
+	 * @param id
+	 * @return 확인 숫자 1 : 성공 / -1: 실패
+	 * @throws Exception
+	 */
 	public int modify_meminfo(String id)throws Exception;
 	
 	
 	//메인화면
 	//리턴값 : 현재 로그인 된 회원의 진행중인 챌린지
+	/**
+	 * 
+	 * @param id
+	 * @return sign_together 현재 로그인 된 회원의 진행중인 챌린지
+	 * 
+	 */
 	public Sign_Together main(String id)throws Exception;
 	
 	// 닉네임 중복확인 
 	//리턴값 : 실패시 0 , 성공시 1 
+	/**
+	 * 
+	 * @param id
+	 * @return 확인 숫자 1 : 성공 / -1: 실패
+	 * 
+	 */
 	public int selectByNickname(String id)throws Exception;
 	
 	// 아이디 중복확인
 	//리턴값 : 실패시 0 , 성공시 1  
-	public int selectById() throws Exception;
+	/**
+	 * @param id
+	 * @return 확인 숫자 1 : 성공 / -1: 실패
+	 */
+	public int selectById(String id) throws Exception;
 	
 	// 내가 참여한 챌린지 - 메인에 사용
-	// 
+	/**
+	 * 
+	 * @param id
+	 * @return sign_together 내가 참여한 챌린지
+	 */
 	public Sign_Together selectByTogether(String id)throws Exception;
 	
 	//임박한 챌린지 -메인에 사용
-	// 
+	/**
+	 * 
+	 * @return sign_together 임박한 투게더
+	 */
 	public Sign_Together soon_together()throws Exception;
 
 }
--- dao/TogetherDaoInterface.java
+++ dao/TogetherDaoInterface.java
@@ -10,46 +10,107 @@ import vo.Together;
 public interface TogetherDaoInterface {
 
 	// 모든 투게더 출력
-	// 
+	/**
+	 * 모든 투게더 출력
+	 * @return Together 
+	 * @throws Exception
+	 */
+	
 	public Together togetherList()throws Exception;
 	
 	// 투게더 상세
+	/**
+	 * 투게더 상세
+	 * @param together_info
+	 * @return Together t
+	 */
 	public Together togetherDetail(String together_info)throws Exception;
 	
 	// 인증모아보기
+	/**
+	 * 인증 모아보기
+	 * @return sign_together 
+	 */
+	
 	public Sign_Together photoCollection()throws Exception;
 	
-	// 결제
+	// 결재
 	// 남은 잔액 반환
 	// 0원인 확인 비교 
+	/**
+	 * 결재
+	 * @param together_info
+	 * @return money 잔액
+	 */
 	public int pay(String together_info)throws Exception;
 	
 	// 충전
 	// 충전후 잔액 반환
+	/**
+	 * 
+	 * @param money
+	 * @return money 충전후 잔액
+	 */
 	public int charge(int money)throws Exception;
 	
 	// 챌린지 인증 insert
 	// 모르겠음
+	/**
+	 * 챌린지 인증 추가
+	 * @param ps
+	 * @return Together 
+	 */
 	public Together certifyT(Photo_Shoot ps)throws Exception;
 
 	// 투게더 생성
-	// 
+	/**
+	 * 
+	 * @param Together t
+	 * @return Together 
+	 */
 	public Together createT(Together t)throws Exception;
 	
 	// 투게더 수정
+	/**
+	 * 투게더 수정
+	 * @param t
+	 * @return Together
+	 */
 	public Together updateT(Together t)throws Exception;
 
 	// 챌린지를 여러번하는지 체크
 	// 현재 날짜, 아이디, 투게더 정보
+	/**
+	 * 
+	 * @param day
+	 * @param id
+	 * @param together_no
+	 * @return 이미했으면 :1 / 하지 않았으면 :0
+	 */
 	public int checkPS(Date day, String id, String together_no) throws Exception;
 
-	// 챌린지 이름 검색
+	// 투게더 이름 검색
+	/**
+	 * 투게더 이름 검색
+	 * @param nickname
+	 * @return List<Photo_Shoot>
+	 */
 	public List<Photo_Shoot> name_vw(String nickname) throws Exception;
 	
 	// 카테고리 검색
+	/**
+	 * 카테고리 검색
+	 * @param cName
+	 * @return List<Photo_Shoot>
+	 */
 	public List<Photo_Shoot> category_vw(String cName) throws Exception;
 	
 	// 아이디 검색 
+	/**
+	 * 아이디 검색
+	 * @param id
+	 * @return List<Photo_Shoot>
+	 */
 	public List<Photo_Shoot> id_vw(String id) throws Exception;
 
 }
